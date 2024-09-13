2c0cf4fa3948d8a5ce4e0431ef15c4ecfdcddcdc
soobean02 <gnh09092@gmail.com>
2024-09-13 23:54:12 +0900

    header
    (임시) 추후 수정

---
+++ travel_log_front/src/compnent/common/Header.js
@@ -0,0 +1,90 @@
+import { Link } from "react-router-dom";
+import "./header_footer.css";
+const Header = () => {
+  return (
+    <header class="header">
+      <div>
+        <div class="logo">
+          <Link to="#">Travel Log</Link>
+        </div>
+        <nav class="nav">
+          <ul>
+            <li>
+              <Link to="#">지역</Link>
+              <ul class="sub-menu">
+                <li>
+                  <Link to="#">서울</Link>
+                </li>
+                <li>
+                  <Link to="#">인천</Link>
+                </li>
+                <li>
+                  <Link to="#">경기</Link>
+                </li>
+                <li>
+                  <Link to="#">부산</Link>
+                </li>
+                <li>
+                  <Link to="#">대전</Link>
+                </li>
+                <li>
+                  <Link to="#">대구</Link>
+                </li>
+                <li>
+                  <Link to="#">울산</Link>
+                </li>
+                <li>
+                  <Link to="#">세종</Link>
+                </li>
+                <li>
+                  <Link to="#">광주</Link>
+                </li>
+                <li>
+                  <Link to="#">강원</Link>
+                </li>
+                <li>
+                  <Link to="#">충북</Link>
+                </li>
+                <li>
+                  <Link to="#">충남</Link>
+                </li>
+                <li>
+                  <Link to="#">경북</Link>
+                </li>
+                <li>
+                  <Link to="#">경남</Link>
+                </li>
+                <li>
+                  <Link to="#">전남</Link>
+                </li>
+                <li>
+                  <Link to="#">제주</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="#">상품</Link>
+            </li>
+            <li>
+              <Link to="#">커뮤니티</Link>
+            </li>
+            <li>
+              <Link to="#">고객센터</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <ul class="user-menu">
+          <li>
+            <Link to="#">로그인</Link>
+          </li>
+          <li>
+            <Link to="#">회원가입</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
