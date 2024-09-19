cc5b0ad61a41016a11f12c4158cf7d1f15e12507
awfgmmz <skysiwoo5@naver.com>
2024-09-19 17:08:09 +0900

    webconfig 수정

--- travel_log_back/src/main/java/kr/co/iei/WebConfig.java
+++ travel_log_back/src/main/java/kr/co/iei/WebConfig.java
@@ -1,12 +1,25 @@
 package kr.co.iei;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class WebConfig implements WebMvcConfigurer{
 	@Value("${file.root}")
 	private String root;
+	
+	@Bean
+	public BCryptPasswordEncoder bcrypt() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/","classpath:/static/");
+	}
 	
 }
