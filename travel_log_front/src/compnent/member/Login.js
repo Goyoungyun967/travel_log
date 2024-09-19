463d37cfa3ef0c58c2de46d83ce714837b365df4
limhm1221 <limhm1221@naver.com>
2024-09-19 19:40:11 +0900

    css수정

--- travel_log_front/src/compnent/member/Login.js
+++ travel_log_front/src/compnent/member/Login.js
@@ -6,7 +6,7 @@ import { FormControl, FormLabel, RadioGroup } from "@mui/material";
 
 const Login = () => {
   return (
-    <div className="main-container">
+    <div>
       <div className="radio-group">
         <FormControl>
           <RadioGroup row defaultValue="female" name="radio-buttons-group">
@@ -23,32 +23,30 @@ const Login = () => {
           </RadioGroup>
         </FormControl>
       </div>
-      <div className="container">
-        <h2 className="head-login">로그인</h2>
-        <form className="form">
-          <div className="input-group">
-            <label htmlFor="id" className="id">
-              아이디 :
-            </label>
-            <input type="id" id="id" />
+      <div className="login-container">
+        <div className="login-wrap">
+          <h2 className="head-login">로그인</h2>
+          <form className="form">
+            <div className="input-group">
+              <label htmlFor="memberId">아이디 :</label>
+              <input type="text" name="memberId" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="memberPw">비밀번호:</label>
+              <input type="password" className="memberPw" id="memberPw" />
+            </div>
+            <button type="submit" className="login-btn">
+              로그인
+            </button>
+          </form>
+          <div className="link-group">
+            <Link to="/searchId" className="link-btn">
+              아이디 찾기
+            </Link>
+            <Link to="/searchpw" className="link-btn">
+              비밀번호 찾기
+            </Link>
           </div>
-          <div className="input-group">
-            <label htmlFor="password" className="pw">
-              비밀번호:
-            </label>
-            <input type="password" id="password" />
-          </div>
-          <button type="submit" className="login-btn">
-            로그인
-          </button>
-        </form>
-        <div className="link-group">
-          <Link to="/searchId" className="link">
-            아이디 찾기
-          </Link>
-          <Link to="/searchpw" className="link">
-            비밀번호 찾기
-          </Link>
         </div>
       </div>
     </div>
