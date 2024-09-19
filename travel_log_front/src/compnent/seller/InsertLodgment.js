7fa11e8fa8e0d05ceca7f69dd6425e71a7e4b2ec
soobean02 <gnh09092@gmail.com>
2024-09-19 14:33:56 +0900

    프론트 - 숙소 등록

--- travel_log_front/src/compnent/seller/InsertLodgment.js
+++ travel_log_front/src/compnent/seller/InsertLodgment.js
@@ -1,12 +1,64 @@
+import { useState } from "react";
 import "./css/insert_lodgment.css";
 
 const InsertLodgment = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const yesHotel = () => {
+    setIsVisible(true);
+  };
+  const noHotel = () => {
+    setIsVisible(false);
+  };
+  const search = () => {};
   return (
     <div className="contanier">
       <form>
         <div className="box-wrap box-radius">
           <div className="box">
-            <img src="/img/default_img.png" alt="사진 추가" />
+            <div className="box-img">
+              <img src="/img/default_img.png" alt="사진 추가" />
+            </div>
+          </div>
+          <div className="Q">
+            <h3>찾으시는 호텔이 있으신가요?</h3>
+            <button type="button" className="btn primary" onClick={yesHotel}>
+              YES
+            </button>
+            <button className="btn primary" onClick={noHotel}>
+              No
+            </button>
+            <div
+              className="search-hotel"
+              style={{ display: isVisible ? "block" : "none" }}
+            >
+              <div className="input">
+                <input type="text" />
+                <button onClick={search}>
+                  <div className="material-icons">search</div>
+                </button>
+                <p className="msg">최소 두글자 이상..</p>
+              </div>
+              <div className="search-item-wrap">
+                <div className="search-item">
+                  <div className="item-title">000호텔</div>
+                  <div className="item-addr">
+                    주소주소주소주소주소주소주소주소주소주소
+                  </div>
+                </div>
+                <div className="search-item">
+                  <div className="item-title">000호텔</div>
+                  <div className="item-addr">
+                    주소주소주소주소주소주소주소주소주소주소
+                  </div>
+                </div>
+                <div className="search-item">
+                  <div className="item-title">000호텔</div>
+                  <div className="item-addr">
+                    주소주소주소주소주소주소주소주소주소주소
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="box">
             <div className="input-wrap">
@@ -26,7 +78,7 @@ const InsertLodgment = () => {
                   <label htmlFor="checkIn">체크인</label>
                 </div>
                 <div className="input">
-                  <input type="text" id="checkIn" />
+                  <input type="time" />
                 </div>
               </div>
               <div className="input-item">
@@ -34,16 +86,16 @@ const InsertLodgment = () => {
                   <label htmlFor="checkOut">체크아웃</label>
                 </div>
                 <div className="input">
-                  <input type="text" id="checkOut" />
+                  <input type="time" />
                 </div>
               </div>
             </div>
           </div>
           <div className="box box-notice">
             <h5>공지사항</h5>
-            <div className="toast">토스트 에디터 들어갈 자리</div>
+            <div className="toast">에디터 들어갈 자리</div>
           </div>
-          <button className="btn primary">수정 완료</button>
+          <button className="btn primary">등록하기</button>
         </div>
       </form>
     </div>
