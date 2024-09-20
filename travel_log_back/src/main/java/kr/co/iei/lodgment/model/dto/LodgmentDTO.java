f34eacff0778b9df0346c7cf3909aa72bb8df651
Heesuya <goqkddnf@naver.com>
2024-09-20 18:34:50 +0900

    Lodgment 숙소상세페이지, 결제페이지 UI/UX

--- travel_log_back/src/main/java/kr/co/iei/lodgment/model/dto/LodgmentDTO.java
+++ travel_log_back/src/main/java/kr/co/iei/lodgment/model/dto/LodgmentDTO.java
@@ -19,18 +19,18 @@ public class LodgmentDTO {
 	private int sellerNo;
 	@Schema(description = "숙소 타입 번호",type="int")
 	private int lodgmentTypeNo;
-	@Schema(description = "숙소 이름",type="String")
+	@Schema(description = "숙박업체명",type="String")
 	private String lodgmentName;
-	@Schema(description = "숙소 이름",type="String")
+	@Schema(description = "주소",type="String")
 	private String lodgmentAddr;
-	@Schema(description = "숙소 이미지 경로",type="String")
+	@Schema(description = "이미지 경로",type="String")
 	private String lodgmentImgPath;
-	@Schema(description = "숙소 성급",type="int")
+	@Schema(description = "성급",type="int")
 	private int lodgmentStarGrade;
-	@Schema(description = "숙소 공지사항",type="String")
+	@Schema(description = "공지사항",type="String")
 	private String lodgmentNotice;
-	@Schema(description = "숙소 체크인",type="String")
+	@Schema(description = "체크인 시간",type="String")
 	private String lodgmentCheckIn;
-	@Schema(description = "숙소 체크아웃",type="String")
+	@Schema(description = "체크아웃 시간",type="String")
 	private String lodgmentCheckOut;
 }
---
+++ travel_log_front/src/compnent/lodgment/LodgmentLoomDetail.js
@@ -0,0 +1,32 @@
+const LodgmentLoomDetail = () => {
+  return (
+    <div className="lodgment-loom-type">
+      <div>
+        <table className="lodgment-loom-table">
+          <tbody>
+            <tr>
+              <td width={"40%"}>
+                <div className="lodgment-loom-type-img">
+                  <img src="/image/nesthotel.jpg" text="임시사진" />
+                </div>
+              </td>
+              <td width={"60%"}>
+                <table>
+                  <tbody>
+                    <tr>방이름</tr>
+                    <tr>체크아웃</tr>
+                    <tr>기준 : 명</tr>
+                    <tr>00월 00일 00월 00일</tr>
+                    <tr>가격 : 0000원</tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default LodgmentLoomDetail;
