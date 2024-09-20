1e4879d4d54545e4f7af6096f6f10f94d78dc468
soobean02 <gnh09092@gmail.com>
2024-09-20 11:14:09 +0900

    숙소 이미지 등록 / 주소 찾기 등록

--- travel_log_front/src/compnent/seller/InsertLodgment.js
+++ travel_log_front/src/compnent/seller/InsertLodgment.js
@@ -4,6 +4,30 @@ import axios from "axios";
 import DaumPostcode from "react-daum-postcode";
 
 const InsertLodgment = () => {
+  // back으로 보내는 이미지
+  const [lodgmentImg, setLodgmentImg] = useState(null);
+  // 미리보기용 이미지
+  const [viewImg, setViewImg] = useState(null);
+  const lodgmentImgRef = useRef(null);
+  const changeLodgmentImg = (e) => {
+    //요소들이 겹쳐있는 상태에서 해당 요소를 선택할 때는 currentTarget(target을사용하면 여러요소가 한번에 선택)
+    const files = e.currentTarget.files; // ※배열 처럼 보이지만 배열 아님※
+    if (files.length !== 0 && files[0] !== 0) {
+      //썸네일 파일 객체를 글작성 시 전송하기위한 값 저장
+      setLodgmentImg(files[0]);
+      //화면에 썸네일 미리보기
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onloadend = () => {
+        setViewImg(reader.result);
+      };
+    } else {
+      // 썸네일 있던걸 없애면 null로 처리 ()
+      setLodgmentImg(null);
+      setViewImg(null);
+    }
+  };
+
   const backServer = process.env.REACT_APP_BACK_SERVER;
   // YES 나 NO를 누르면 search-hotel이 보이거나 안보이게 하기
   const [isVisible, setIsVisible] = useState(false);
@@ -38,23 +62,60 @@ const InsertLodgment = () => {
       });
   };
 
-  console.log(lodgmentType);
-  const addrApi = () => {
-    // setIsPostcodeVisible(true);
+  // 주소
+  const [addrBool, setAddrBool] = useState(false);
+  const addrOk = () => {
+    setAddrBool((prev) => !prev);
   };
-  const addrComplete = (data) => {
-    console.log(data); // 선택한 주소 데이터를 처리
-    // setIsPostcodeVisible(false); // 주소 선택 후 컴포넌트를 숨기려면 사용
+  const [zoneCode, setZoneCode] = useState(""); // 우편번호
+  const [address, setAddress] = useState(""); // 주소
+  const [deAddress, setDeAddress] = useState(""); // 상세주소
+  // 우편번호랑 주소 input에 넣기
+  const completeHandler = (data) => {
+    const { address, zonecode } = data;
+    setZoneCode(zonecode);
+    setAddress(address);
   };
-
+  const changeDeAddress = (e) => {
+    setDeAddress(e.target.value);
+  };
   return (
     <div className="contanier insert-lodgment">
       <form>
         <div className="box-wrap box-radius">
           <div className="box">
             <div className="box-img">
-              <img src="/img/default_img.png" alt="사진 추가" />
+              {viewImg ? (
+                <img
+                  src={viewImg}
+                  onClick={() => {
+                    lodgmentImgRef.current.click();
+                  }}
+                />
+              ) : viewImg ? (
+                // 이미지를 띄울 때 서버주소/경로/
+                <img
+                  src={`${backServer}/seller/lodgment/${viewImg}`}
+                  onClick={() => {
+                    lodgmentImgRef.current.click();
+                  }}
+                />
+              ) : (
+                <img
+                  src="/image/lodgment_default_img.png"
+                  onClick={() => {
+                    lodgmentImgRef.current.click();
+                  }}
+                ></img>
+              )}
             </div>
+            <input
+              ref={lodgmentImgRef}
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              onChange={changeLodgmentImg}
+            ></input>
           </div>
           <div className="Q">
             <h3>찾으시는 호텔이 있으신가요?</h3>
@@ -75,7 +136,6 @@ const InsertLodgment = () => {
                   value={searchInput}
                   onChange={searchInputChange}
                 />
-                <p className="msg">최소 두글자 이상..</p>
               </div>
               <div className="search-item-wrap">
                 <div className="search-item">
@@ -111,9 +171,35 @@ const InsertLodgment = () => {
               </div>
               <div className="input-item">
                 <div className="addr-api">
-                  <button type="button" className=" btn primary">
+                  <button
+                    type="button"
+                    className=" btn primary"
+                    onClick={addrOk}
+                  >
                     주소찾기
                   </button>
+                  <div className="addr-block">
+                    <div className="addr-search-api">
+                      {addrBool ? (
+                        <DaumPostcode onComplete={completeHandler} />
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                    <div className="addr-api-input">
+                      <label htmlFor="addrCode">우편번호</label>
+                      <input type="text" id="addrText" value={zoneCode} />
+                      <label htmlFor="addrText">주소</label>
+                      <input type="text" id="addrText" value={address} />
+                      <label htmlFor="addrDeText">상세 주소</label>
+                      <input
+                        type="text"
+                        id="addrDeText"
+                        value={deAddress}
+                        onChange={changeDeAddress}
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="input-item">
@@ -193,7 +279,7 @@ const InsertLodgment = () => {
           </div>
           <div className="box box-notice">
             <h5>공지사항</h5>
-            <div className="toast">에디터 들어갈 자리</div>
+            <div className="editor">에디터 들어갈 자리</div>
           </div>
           <button className="btn primary">등록하기</button>
         </div>
