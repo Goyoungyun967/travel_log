bd03a3c74ab21013243dd093052440d3fe985dee
Goyoungyun967 <mda208@naver.com>
2024-09-29 23:19:00 +0900

    좋아요 기능 우선 주석처리

--- travel_log_back/src/main/java/kr/co/iei/board/model/dao/BoardDao.java
+++ travel_log_back/src/main/java/kr/co/iei/board/model/dao/BoardDao.java
@@ -37,15 +37,27 @@ public interface BoardDao {
 	List<BoardFileDTO> selectBoardFile(int[] delBoardFileNo);
 
 	int deleteBoardFile(int[] delBoardFileNo);
-
-	int insertLikeBoard(int boardNo, int memberNo);
-
-	int selectLikeBoard(int boardNo);
-
-	int deleteUnlikeBoard(int boardNo, int memberNo);
-	
-	int selectUnlikeBoard(int boardNo);
-
+//
+//	int insertLikeBoard(int boardNo, int memberNo);
+//
+//	int selectLikeBoard(int boardNo);
+//
+//	int deleteUnlikeBoard(int boardNo, int memberNo);
+//	
+//	int selectUnlikeBoard(int boardNo);
+//	<insert id="insertLikeBoard">
+//	insert into board_like values(member_no=#{memberNo},boardNo=#{boardNo})
+//</insert>
+//<select id="selectLikeBoard" resultType="int">
+//	select count(*) from board_like where board_no = #{boardNo}  
+//</select>
+//
+//<delete id="deleteUnlikeBoard">
+//	delete from board_like where board_no=#{boardNo} and member_No=#{memberNo}
+//</delete>
+//<select id="selectUnlikeBoard" resultType="int">
+//	select count(*) from board_like where board_no = #{boardNo}
+//</select>
 	
 	
 
