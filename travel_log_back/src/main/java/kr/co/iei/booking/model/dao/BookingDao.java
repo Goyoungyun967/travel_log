b4bf621ab14d7210f1c6f796e364fa78ab94227c
limhm1221 <limhm1221@naver.com>
2024-10-06 20:19:40 +0900

    나의 예약정보 완

--- travel_log_back/src/main/java/kr/co/iei/booking/model/dao/BookingDao.java
+++ travel_log_back/src/main/java/kr/co/iei/booking/model/dao/BookingDao.java
@@ -1,12 +1,19 @@
 package kr.co.iei.booking.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.co.iei.booking.model.dto.BookingDTO;
+import kr.co.iei.util.PageInfo;
 
 @Mapper
 public interface BookingDao {
 
 	int insertBooking(BookingDTO bookingInfo);
+
+	int totalCount();
+
+	List selectBookingList(PageInfo pi);
 
 }
